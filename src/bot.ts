a4f7991d9917d868c68564f10421ecfe6928742f
komoza <maximkomoza@gmail.com>
2025-07-01 14:45:49 +0800

    Remove unused `who` command setup from bot initialization logic.

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,5 @@
 import { Telegraf } from 'telegraf';
 import * as dotenv from 'dotenv';
-import { setupWhoCommand } from './commands/who';
 import { setupRegCommand } from "./commands/reg";
 import {setupHelpCommand} from "./commands/help";
 import {setupPingCommand} from "./commands/ping";
@@ -9,7 +8,6 @@ dotenv.config();
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
 
-// setupWhoCommand(bot);
 setupRegCommand(bot);
 setupHelpCommand(bot);
 setupPingCommand(bot);
