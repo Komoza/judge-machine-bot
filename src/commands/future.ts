303292aaf5d5326839968eea9e9d93b21b3686b9
komoza <maximkomoza@gmail.com>
2025-07-07 22:55:34 +0800

    update predictions

--- src/commands/future.ts
+++ src/commands/future.ts
@@ -1,9 +1,32 @@
-import {getRandom} from "../utils/random";
-import {Telegraf} from "telegraf";
-import {PREDICTIONS} from "../utils/predictions";
+import { getRandom } from '../utils/random';
+import { Telegraf } from 'telegraf';
+import { PREDICTIONS } from '../utils/predictions';
+import {getUsersByChat} from "../store/chat-user";
 
 export const setupFutureCommand = (bot: Telegraf) => {
-  bot.command('future', (ctx) => {
-    ctx.reply(`${getRandom(PREDICTIONS)}`);
+  bot.command('future', async (ctx) => {
+    const chatId = ctx.chat.id;
+    const authorId = ctx.from.id.toString();
+
+    try {
+      const users = await getUsersByChat(chatId);
+      const others = users.filter((u) => u.id !== authorId && !u.is_bot);
+
+      if (others.length === 0) {
+        return ctx.reply('Ты тут один, сам себе и предскажи.');
+      }
+
+      const randomUser = getRandom(others);
+      const displayName = randomUser.username
+        ? `@${randomUser.username}`
+        : `${randomUser.first_name}${randomUser.last_name ? ' ' + randomUser.last_name : ''}`;
+
+      const prediction = getRandom(PREDICTIONS).replace('${user}', displayName);
+
+      ctx.reply(prediction);
+    } catch (err) {
+      console.error('Ошибка в future:', err);
+      ctx.reply('Что-то пошло по пизде с предсказаниями.');
+    }
   });
 };
