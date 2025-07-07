39e717944f0f8da7ed81280ce450e371cf50185b
komoza <maximkomoza@gmail.com>
2025-07-07 22:15:43 +0800

    Reintroduce `/who` command: add setup logic during bot initialization

--- src/bot.ts
+++ src/bot.ts
@@ -5,11 +5,13 @@ import {setupHelpCommand} from "./commands/help";
 import {setupPingCommand} from "./commands/ping";
 import {setupFutureCommand} from "./commands/future";
 import {setupPoolCommand} from "./commands/pool";
+import {setupWhoCommand} from "./commands/who";
 
 dotenv.config();
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
 
+setupWhoCommand(bot)
 setupRegCommand(bot);
 setupHelpCommand(bot);
 setupPingCommand(bot);
